7ed2c266fc3ab81937d23d38eff8893f58d223af
Alejandro Rupérez Hernando <alexruperez@gmail.com>
2014-04-18 17:49:54 +0200

    Initial Commit

---
+++ ARWebServerActivityExample/ARAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ARAppDelegate.h
+//  ARWebServerActivityExample
+//
+//  Created by alexruperez on 18/04/14.
+//  Copyright (c) 2014 alexruperez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ARAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ARWebServerActivityExample/ARViewController.m
@@ -0,0 +1,29 @@
+//
+//  ARViewController.m
+//  ARWebServerActivityExample
+//
+//  Created by alexruperez on 18/04/14.
+//  Copyright (c) 2014 alexruperez. All rights reserved.
+//
+
+#import "ARViewController.h"
+
+@interface ARViewController ()
+
+@end
+
+@implementation ARViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
